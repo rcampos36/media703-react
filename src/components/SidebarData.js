c090e66678ef2cf1525aec6bf45b0c0e181a7398
Roger Campos <=>
2021-01-13 12:46:40 -0500

    conneted netlify

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -5,7 +5,7 @@ import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
 	{
-		title: 'Home',
+		title: 'Portfolio',
 		path: '/',
 		icon: <AiIcons.AiFillHome />,
 		cName: 'nav-text'
