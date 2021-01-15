f2655ced6de898e848c523714f5bc62e210cb6e4
Roger Campos <=>
2021-01-15 16:35:41 -0500

    adding content styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import * as RiIcons from "react-icons/ri";
 
 export class Header extends React.Component {
 	render() {
 		return (
 			<div className="">
-				<div class="logo-main text-4xl">MEDIA703</div>
+				<div class="logo-main text-5xl flex"><span className="border-r border-gray-300 mr-2 pr-2"><RiIcons.RiStackLine /></span>703</div>
 			</div>
 		)
 	}
