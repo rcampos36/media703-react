433fd73add575da4d6aeba16f36912ed019bc233
Roger Campos <=>
2021-01-17 15:45:12 -0500

    adding content styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import * as RiIcons from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 export class Header extends React.Component {
 	render() {
 		return (
 			<div className="">
-				<div class="logo-main text-5xl flex antialiased"><span className="border-r border-gray-300 mr-2 pr-2"><RiIcons.RiStackLine /></span>703</div>
+			<router>
+				<div class="logo-main text-5xl flex antialiased"><Link to="/" className="flex"><span className="border-r border-gray-300 mr-2 pr-2"><RiIcons.RiStackLine /></span>703</Link></div>
+			</router>
 			</div>
 		)
 	}
